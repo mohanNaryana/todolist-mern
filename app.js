edaa7ba2c1a80fe59f2f209177201fa0c78b6810
mohanNaryana <mohankrishnateja2022@gmail.com>
2024-10-19 18:39:37 +0530

    added put and delete routes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.get("/tasks",async(req,res)=>{
     try{
         const todo = await prisma.Todo.findMany()
-        res.status(200).json(todos)
+        res.status(200).json(todo)
     }catch(error){
         res.status(500).json({ error: 'Failed to fetch tasks' });
     }
@@ -31,6 +31,48 @@ app.post("/tasks",async (req,res)=>{
     }
     catch(error){
         res.status(500).json({ error: 'Failed to create task' });
+    }
+})
+
+
+
+app.put("/tasks/:id",async(req,res)=>{
+    const {id} = req.params
+    const { title , completed } = req.body
+
+    try{
+        const todo = await prisma.Todo.update({
+            where : {id},
+            data : {
+                title,
+                completed,
+            }
+        })
+        res.status(200).json({
+            msg : "updated successfully"
+        })
+    }
+    catch(error){
+        res.status(500).json({ error : 'failed to update task'})
+    }
+})
+
+
+app.delete("/tasks/:id",async(req,res)=>{
+    const {id} = req.params
+
+    try{
+        const todo = await prisma.Todo.delete({
+            where : {
+                id,
+            }
+        })
+        res.status(200).json({
+            msg : "deleted successfully"
+        })
+    }
+    catch(error){
+        res.status(500).json({error : "failed to delete"})
     }
 })
 
