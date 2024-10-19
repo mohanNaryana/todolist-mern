473d23d4f17689662072c9dfc2634f0c34e8e109
mohanNaryana <mohankrishnateja2022@gmail.com>
2024-10-19 17:55:05 +0530

    added post and get route

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
+
 const express = require('express')
 const app = express()
+const{ PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 app.use(express.json())
 
 
-app.get("/",(req,res)=>{
-    res.send("hello iam mohan")
+app.get("/tasks",async(req,res)=>{
+    try{
+        const todo = await prisma.Todo.findMany()
+        res.status(200).json(todos)
+    }catch(error){
+        res.status(500).json({ error: 'Failed to fetch tasks' });
+    }
+})
+
+
+app.post("/tasks",async (req,res)=>{
+    const { title } = req.body
+
+    try{
+        const todo =  await prisma.Todo.create({
+            data : {
+                title 
+            }
+        })
+        res.json({
+            msg : "created successfully"
+        })
+    }
+    catch(error){
+        res.status(500).json({ error: 'Failed to create task' });
+    }
 })
 
 
