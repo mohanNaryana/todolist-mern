d0a5fba273620d34b149a36997d45150fbe7395a
mohanNaryana <mohankrishnateja2022@gmail.com>
2024-10-20 14:55:16 +0530

    added cards component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Add from "./components/Add"
+import Cards from "./components/Cards"
 
 
 function App() {
@@ -6,8 +7,10 @@ function App() {
   return (
     <>
       <div className="bg-gray-900 h-screen">
-        <h1 className="flex justify-center text-5xl text-blue-500">MY TODOS</h1>
+        <h1 className="flex justify-center text-5xl text-blue-500 ">MY TODOS</h1>
         <Add /> 
+        <hr className="border-gray-400 my-4" />
+        <Cards tasks={"eat banana"} date={"2-4-3"} />
       </div>
     </>
   )
--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,7 +1,19 @@
-export default function Cards() {
+export default function Cards({ tasks, date }) {
     return (
-        <div>
-            
+        <div className="flex justify-center items-center mt-5">
+            <div className="flex justify-between items-center space-x-2 p-4 bg-gray-900 border border-gray-700 rounded-lg w-full max-w-lg shadow-lg">
+                <input
+                    type="checkbox"
+                    className="mx-3 border border-gray-600 bg-gray-800 text-white p-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out"
+                />
+                <span className="flex-grow text-white">{tasks}</span>
+                
+                
+                <div className="flex flex-col items-end">
+                    <span className="text-gray-400 text-xs">Updated at</span> 
+                    <span className="text-gray-400 text-xs">{date}</span>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
